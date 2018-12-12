a1b5691d48f684232ad8383d5df8c90b9e292958
mbag24 <40617051+michaelbaggett@users.noreply.github.com>
2018-12-12 11:33:54 -0600

    commiting working code, not ready to be pushed yett

--- Project/routes/apiRoutes.js
+++ Project/routes/apiRoutes.js
@@ -1,24 +1,42 @@
 var db = require("../models");
 
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
-
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
+module.export = function(app) {
+  app.get("/api/users", function(req, res) {
+    // db.User.findAll({}).then(function(results){
+    //   res.json(results);
+    res.send("API INFO WILL GO HERE :)");
   });
 };
+
+
+
+
+
+// module.exports = function(app) {
+//   // Get all examples
+//   app.get("/api/examples", function(req, res) {
+//     db.Example.findAll({}).then(function(dbExamples) {
+//       res.json(dbExamples);
+//     });
+//   });
+
+//   // Create a new User
+//   app.post("/api/login", function(req, res) {
+//     db.User.create({
+//       email: req.body.email,
+//       password: req.body.password
+//     }).then(function() {
+//       res.redirect(307, "/api/login");
+//     }).catch(function(err){
+//       console.log(err);
+//       res.json(err);
+//     });
+//   });
+
+//   // Delete an example by id
+//   app.delete("/api/examples/:id", function(req, res) {
+//     db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+//       res.json(dbExample);
+//     });
+//   });
+// };
--- Project/routes/htmlRoutes.js
+++ Project/routes/htmlRoutes.js
@@ -1,27 +1,44 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    db.User.findAll({}).then(function(results){
+    res.send("Here is our landing page, which is also our login page!");
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+
+  app.get("/signup", function(req, res){
+    res.send("Here is our signup page!");
   });
+
+  app.get("/login", function(req, res){
+    res.send("Here is our login page, this will redirect to the / route!");
+  });
+
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
   });
 };
+
+
+// module.exports = function(app) {
+//   // Load index page
+//   app.get("/", function(req, res) {
+//     db.Example.findAll({}).then(function(dbExamples) {
+//       res.render("index", {
+//         msg: "Welcome!",
+//         examples: dbExamples
+//       });
+//     });
+//   });
+
+//   // Load example page and pass in an example by id
+//   app.get("/example/:id", function(req, res) {
+//     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+//       res.render("example", {
+//         example: dbExample
+//       });
+//     });
+//   });

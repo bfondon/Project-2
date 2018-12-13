a5425670cb169a251b053657bffb49103877c865
mbag24 <40617051+michaelbaggett@users.noreply.github.com>
2018-12-13 13:04:55 -0600

    added passport, authentication works, user is taken to a dummy home page when logged in

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,14 @@
 var db = require("../models");
 var path = require("path");
 
+var isAuthenticated = require("../config/Authentication_Middleware/isAuthenticated");
+
 module.exports = function(app) {
 
   app.get("/", function(req, res) {
+    if (req.user){
+      res.redirect("/members");
+    }
     res.sendFile(path.join(__dirname, "../public/landing.html"));
   });
 
@@ -13,8 +18,15 @@ module.exports = function(app) {
   });
 
   app.get("/login", function(req, res){
+    if (req.user) {
+      res.redirect("/members");
+    }
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
+
+  app.get("/members", isAuthenticated, function(req, res){
+    res.sendFile(path.join(__dirname, "../public/members.html"));
+  })
 
 
   // Render 404 page for any unmatched routes
